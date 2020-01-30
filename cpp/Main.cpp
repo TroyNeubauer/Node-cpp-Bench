a5b57f15e775c9151c23be5c05c59b14422a5e02
Troy Neubauer <troyneubauer@gmail.com>
2020-01-29 23:47:54 -0500

    Added CI files

--- cpp/Main.cpp
+++ cpp/Main.cpp
@@ -16,13 +16,13 @@ spdlog::logger* logger;
 char* buf;
 const int BUF_SIZE = 1024 * 1024;
 
-void CopyFile(const char* fileName, int times)
+void CopyFileWithTimes(const char* fileName, int times)
 {
 	logger->info("Copying file {} {} times", fileName, times);
 
 	std::uint64_t size;
 	TUtil::FileError error;
-	const char* baseFileName = "2m.json";
+	const char* baseFileName = "samples/2m.json";
 	const char* file = reinterpret_cast<const char*>(TUtil::FileSystem::MapFile(baseFileName, TUtil::READ, size, &error));
 
 	if (error != TUtil::FileError::NONE)
@@ -52,14 +52,14 @@ int main(int argc, char* const argv[])
 
 	logger = new spdlog::logger("CPP", { coreStdOut });
 
-	if (TUtil::FileSystem::Exists("20m.txt"))
+	if (TUtil::FileSystem::Exists("samples/20m.txt"))
 		logger->info("Using cached 20mb, 60mb and 100mb files");
 	else
 	{
 		logger->info("Creating files:");
-		CopyFile("20m.txt", 10);
-		CopyFile("60m.txt", 30);
-		CopyFile("100m.txt", 50);
+		CopyFileWithTimes("samples/20m.txt", 10);
+		CopyFileWithTimes("samples/60m.txt", 30);
+		CopyFileWithTimes("samples/100m.txt", 50);
 	}
 
 	int result = Catch::Session().run(argc, argv);
@@ -71,7 +71,9 @@ int main(int argc, char* const argv[])
 	return result;
 }
 
-const char* ReadBenchTUtil(const char* fileName)
+//TUtil Memory Mapped file IO
+
+const char* ReadMappedTUtil(const char* fileName)
 {
 	std::uint64_t size;
 	TUtil::FileError error;
@@ -87,7 +89,7 @@ const char* ReadBenchTUtil(const char* fileName)
 	return file;
 }
 
-const char* CopyBenchTUtil(const char* fileName)
+const char* CopyMappedTUtil(const char* fileName)
 {
 	std::uint64_t size;
 	TUtil::FileError error;
@@ -111,15 +113,17 @@ const char* CopyBenchTUtil(const char* fileName)
 	return file;
 }
 
-//C file IO
+//C file IO (buffered)
 
-const char* ReadBenchBufferedC(const char* fileName)
+const char* ReadBufferedC(const char* fileName)
 {
 	std::uint64_t size = TUtil::FileSystem::FileSize(fileName);
 
 	char* buf = new char[1024 * 1024];
 	std::size_t bytesRead;
 	FILE* file = fopen(fileName, "rb");
+	if (!file) { logger->error("Cannot open file {}", fileName); return nullptr; }
+
 	do
 	{
 		bytesRead = fread(buf, 1, 1024 * 1024, file);
@@ -131,27 +135,15 @@ const char* ReadBenchBufferedC(const char* fileName)
 	return nullptr;
 }
 
-const char* ReadBenchAllC(const char* fileName)
-{
-	std::uint64_t size = TUtil::FileSystem::FileSize(fileName);
-	char* buf = new char[size];
 
-	FILE* file = fopen(fileName, "rb");
-	fread(buf, 1, size, file);
-	fclose(file);
-
-	delete[] buf;
-	return nullptr;
-}
-
-const char* CopyBenchBufferedC(const char* fileName)
+const char* CopyBufferedC(const char* fileName)
 {
 	std::size_t bytesRead;
 	FILE* in = fopen(fileName, "rb");
-	if (!in) logger->error("Cannot open file {}", fileName);
+	if (!in) { logger->error("Cannot open file {}", fileName); return nullptr; }
 
 	FILE* out = fopen("temp.txt", "wb");
-	if (!out) logger->error("Cannot open file {}", "temp.txt");
+	if (!out) { logger->error("Cannot open file {}", "temp.txt"); return nullptr; }
 
 	do
 	{
@@ -165,7 +157,24 @@ const char* CopyBenchBufferedC(const char* fileName)
 	return nullptr;
 }
 
-const char* CopyBenchAllC(const char* fileName)
+//C file IO (allocates one big buffer the size of the file)
+
+const char* ReadAllC(const char* fileName)
+{
+	std::uint64_t size = TUtil::FileSystem::FileSize(fileName);
+	char* buf = new char[size];
+
+	FILE* file = fopen(fileName, "rb");
+	if (!file) { logger->error("Cannot open file {}", fileName); return nullptr; }
+
+	fread(buf, 1, size, file);
+	fclose(file);
+
+	delete[] buf;
+	return nullptr;
+}
+
+const char* CopyAllC(const char* fileName)
 {
 	std::size_t bytesRead;
 	FILE* in = fopen(fileName, "rb");
@@ -188,18 +197,69 @@ const char* CopyBenchAllC(const char* fileName)
 	return nullptr;
 }
 
-#define TestFunction(name, function, ...)
-TEST_CASE(name) {
-	BENCHMARK(name)
-	{
-		return function(name, __VA_ARGS__);
-	};
-}
 
-template<> void TestFunction<ReadBenchBufferedC>("read C-buffered 5k.json", "5k.json");
+#define TestFunction(name, function, ...)		\
+TEST_CASE(name) {								\
+	BENCHMARK(name)								\
+	{											\
+		function(__VA_ARGS__);					\
+	};											\
+}												\
+												\
+
+
+//========== READ ==========
+//NAME FORMAT: group | strategy | operation
+
+TestFunction("read | C-buffered | 5k.json", ReadBufferedC, "samples/5k.json")
+TestFunction("read | C-buffered | 50k.json", ReadBufferedC, "samples/50k.json")
+TestFunction("read | C-buffered | 500k.json", ReadBufferedC, "samples/500k.json")
+TestFunction("read | C-buffered | 2m.json", ReadBufferedC, "samples/2m.json")
+TestFunction("read | C-buffered | 20m.txt", ReadBufferedC, "samples/20m.txt")
+TestFunction("read | C-buffered | 60m.txt", ReadBufferedC, "samples/60m.txt")
+TestFunction("read | C-buffered | 100m.txt", ReadBufferedC, "samples/100m.txt")
+
+TestFunction("read | C-All | 5k.json", ReadAllC, "samples/5k.json")
+TestFunction("read | C-All | 50k.json", ReadAllC, "samples/50k.json")
+TestFunction("read | C-All | 500k.json", ReadAllC, "samples/500k.json")
+TestFunction("read | C-All | 2m.json", ReadAllC, "samples/2m.json")
+TestFunction("read | C-All | 20m.txt", ReadAllC, "samples/20m.txt")
+TestFunction("read | C-All | 60m.txt", ReadAllC, "samples/60m.txt")
+TestFunction("read | C-All | 100m.txt", ReadAllC, "samples/100m.txt")
+
+TestFunction("read | TUtil-Mapped | 5k.json", ReadMappedTUtil, "samples/5k.json")
+TestFunction("read | TUtil-Mapped | 50k.json", ReadMappedTUtil, "samples/50k.json")
+TestFunction("read | TUtil-Mapped | 500k.json", ReadMappedTUtil, "samples/500k.json")
+TestFunction("read | TUtil-Mapped | 2m.json", ReadMappedTUtil, "samples/2m.json")
+TestFunction("read | TUtil-Mapped | 20m.txt", ReadMappedTUtil, "samples/20m.txt")
+TestFunction("read | TUtil-Mapped | 60m.txt", ReadMappedTUtil, "samples/60m.txt")
+TestFunction("read | TUtil-Mapped | 100m.txt", ReadMappedTUtil, "samples/100m.txt")
 
 //========== COPY ==========
 
+TestFunction("copy | C-buffered | 5k.json", CopyBufferedC, "samples/5k.json")
+TestFunction("copy | C-buffered | 50k.json", CopyBufferedC, "samples/50k.json")
+TestFunction("copy | C-buffered | 500k.json", CopyBufferedC, "samples/500k.json")
+TestFunction("copy | C-buffered | 2m.json", CopyBufferedC, "samples/2m.json")
+TestFunction("copy | C-buffered | 20m.txt", CopyBufferedC, "samples/20m.txt")
+TestFunction("copy | C-buffered | 60m.txt", CopyBufferedC, "samples/60m.txt")
+TestFunction("copy | C-buffered | 100m.txt", CopyBufferedC, "samples/100m.txt")
+
+TestFunction("copy | C-All | 5k.json", CopyAllC, "samples/5k.json")
+TestFunction("copy | C-All | 50k.json", CopyAllC, "samples/50k.json")
+TestFunction("copy | C-All | 500k.json", CopyAllC, "samples/500k.json")
+TestFunction("copy | C-All | 2m.json", CopyAllC, "samples/2m.json")
+TestFunction("copy | C-All | 20m.txt", CopyAllC, "samples/20m.txt")
+TestFunction("copy | C-All | 60m.txt", CopyAllC, "samples/60m.txt")
+TestFunction("copy | C-All | 100m.txt", CopyAllC, "samples/100m.txt")
+
+TestFunction("copy | TUtil-Mapped | 5k.json", CopyMappedTUtil, "samples/5k.json")
+TestFunction("copy | TUtil-Mapped | 50k.json", CopyMappedTUtil, "samples/50k.json")
+TestFunction("copy | TUtil-Mapped | 500k.json", CopyMappedTUtil, "samples/500k.json")
+TestFunction("copy | TUtil-Mapped | 2m.json", CopyMappedTUtil, "samples/2m.json")
+TestFunction("copy | TUtil-Mapped | 20m.txt", CopyMappedTUtil, "samples/20m.txt")
+TestFunction("copy | TUtil-Mapped | 60m.txt", CopyMappedTUtil, "samples/60m.txt")
+TestFunction("copy | TUtil-Mapped | 100m.txt", CopyMappedTUtil, "samples/100m.txt")
 
 //========== JSON ==========
 
---
+++ run.py
@@ -0,0 +1,140 @@
+import sys
+import os
+import platform
+import subprocess
+import shutil
+from distutils.file_util import copy_file
+from distutils.dir_util import mkpath
+
+def run(command, in_env = None):
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=in_env)
+
+	# Poll process for new output until finished
+	while True:
+		nextline = process.stdout.readline()
+		if len(nextline) == 0 and process.poll() is not None:
+			break
+		sys.stdout.write(nextline.decode("utf-8"))
+
+	exitCode = process.returncode
+
+	if exitCode != 0:
+		print('Command \'' + command + '\' failed!')
+		print('Expected error code 0, got ' + str(exitCode))
+		sys.exit(1)
+
+def copyALlInDir(src, dest):
+	for root, dirs, files in os.walk(src, topdown=False):
+		for name in files:
+			destFile = os.path.join(os.path.join(dest, root[len(src):]), name)
+			print('loop file ' + root + ' ' + name + ' cp to ' + destFile)
+			mkpath(os.path.abspath(os.path.join(destFile, '..')))
+			copy_file(os.path.join(root, name), destFile)
+
+
+if len(sys.argv) != 3:
+	print("Command line arguments either incorrect or not specifyed. Reverting to defaults")
+	osName = platform.system().lower()
+	if osName == "windows":
+		compiler = "msvc"
+	elif osName == "linux":
+		compiler = "gcc"
+	elif osName == "darwin":
+		osName = "macosx"
+		compiler = "clang"
+	else:
+		osName = "unknown"
+
+else:
+	print("Using command line arguments!")
+	osName = sys.argv[1]
+	compiler = sys.argv[2]
+
+
+print('')
+print('osName ' + osName)
+print('compiler ' + compiler)
+print('')
+print('')
+
+
+premakeCommand = ''
+if osName == 'linux':
+	premakeCommand += 'premake5 '
+
+elif osName == 'windows':
+	premakeCommand += 'call vendor\\premake\\bin\\premake5.exe '
+
+elif osName == 'macosx':
+	print('osx is unsupported for now!')
+	sys.exit(1)
+
+else:
+	print('Unknown OS! ' + osName)
+	sys.exit(1)
+
+
+
+if compiler == 'msvc':
+	premakeCommand += '--os=windows --compiler=msc vs2019'
+
+elif compiler == 'gcc':
+	premakeCommand += '--os=' + osName + ' --compiler=gcc gmake2'
+
+elif compiler == 'clang':
+	premakeCommand += '--os=' + osName + ' --compiler=gcc gmake2'
+
+elif compiler == 'emcc':
+	premakeCommand += '--os=emscripten --scripts=Hazel/vendor/premake/scripts gmake2'
+
+else:
+	print('Unknown compiler! ' + compiler)
+	sys.exit(1)
+
+print('running premake: \"' + premakeCommand + '\"')
+run(premakeCommand)
+
+#Project files have been generated compile CPP
+
+if osName == 'windows':
+	command = 'MSBuild.exe /m /p:Configuration=release NodeCppBench.sln'
+else:
+	command = 'make -j2 config=release'
+
+print('running: ' + command)	
+print('compiling...')
+
+if osName == 'windows':
+	s = ';'
+else:
+	s = ':'
+
+env = os.environ.copy()
+origionalPath = env["PATH"]
+env["PATH"] = ''
+
+if compiler == 'emcc':
+	env["PATH"] += os.path.join(os.getcwd(), 'Hazel/emsdk-master') + s;
+	env["PATH"] += os.path.join(os.getcwd(), 'Hazel/emsdk-master/node/12.9.1_64bit/bin') + s
+	env["PATH"] += os.path.join(os.getcwd(), 'Hazel/emsdk-master/fastcomp/emscripten') + s
+
+if osName == 'windows':
+	env["PATH"] += "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin" + s
+
+env["PATH"] += origionalPath
+
+env.pop("CC", None)
+env.pop("CXX", None)
+env.pop("AR", None)
+
+print('env: ' + str(env))
+
+#Compile
+run(command, env)
+
+
+
+
+
+#install python dependencies
+run("pip install plotly")
